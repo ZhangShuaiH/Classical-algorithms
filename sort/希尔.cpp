7965cc3b3fd0b5ce98c19561d709f6bd9eb769ef
张帅 <2010296739@qq.com>
2018-08-14 08:48:14 +0800

    Update 希尔.cpp

--- sort/希尔.cpp
+++ sort/希尔.cpp
@@ -3,7 +3,17 @@
 #include "ConsoleApplication1.h"
 
 using namespace std;
-// 希尔排序
+/**************希尔排序***************
+空间复杂度：O(1)
+时间复杂度：O(n^2)
+不稳定。
+基于插入排序，并有较大改进
+增量序列不要互为倍数
+**************工作原理****************
+希尔排序是把记录按下标的一定增量分组，对每组使用直接插入排序算法排序；
+随着增量逐渐减少，每组包含的关键词越来越多，当增量减至1时，
+整个文件恰被分成一组，算法便终止.
+************************************/
 void shell_sort(int a[], const int size) {
     for (int increment = size / 2; increment >= 1; increment /= 2)
         for (int i = 0; i < size; i++)
@@ -44,5 +54,5 @@ int main()
     print(a, size);
     print(b, size);
 
-	return 0;
+    return 0;
 }
