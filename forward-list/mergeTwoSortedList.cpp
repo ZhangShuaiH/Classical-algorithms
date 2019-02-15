fa9dbdc4c4180a3047f17ed4c61037587d95db2a
张帅 <18143358+ZhangShuaiH@users.noreply.github.com>
2019-02-15 15:01:00 +0800

    Update mergeTwoSortedList.cpp

--- forward-list/mergeTwoSortedList.cpp
+++ forward-list/mergeTwoSortedList.cpp
@@ -7,8 +7,8 @@ struct Node {
     Node* next;
     Node(int val):val(val),next(nullptr){}
 };
-
-Node *mergeTwoSortedList(Node *n1, Node *n2){
+// 方法 1
+Node *mergeTwoSortedList(Node *n1, Node *n2){// 新建临时节点，更易理解
     Node *head = new Node(-1);
     Node *cur = head;
     while (n1 && n2) {
@@ -28,6 +28,28 @@ Node *mergeTwoSortedList(Node *n1, Node *n2){
     cur = nullptr;
     return head;
 }
+// 方法 2
+Node *mergeTwoSortedList1(Node *n1, Node *n2){
+    if(n1==nullptr || n2==nullptr)return n1!=nullptr?n1:n2;
+    Node *head = n1->val<=n2->val ? n1 : n2;
+    Node *cur = head;
+    if(head == n1)n1=n1->next;
+    else n2 = n2->next;
+    while (n1 && n2) {
+        if(n1->val <= n2->val){
+            cur->next = n1;
+            n1 = n1->next;
+        }else {
+            cur->next = n2;
+            n2 = n2->next;
+        }
+        cur = cur->next;
+    }
+    cur->next = n1 != nullptr ? n1 : n2;
+
+    return head;
+}
+
 void printList(Node* head){
     Node *cur = head;
     for(Node *cur=head; cur!=nullptr; cur=cur->next)cout<<cur->val<<",";
@@ -36,12 +58,9 @@ void printList(Node* head){
 
 int main(){
     Node *n1 = new Node(1);
-    n1->next = new Node(3);
-    n1->next->next = new Node(5);
 
     Node *n2 = new Node(2);
     n2->next = new Node(4);
-    n2->next->next = new Node(6);
 
     printList(mergeTwoSortedList(n1, n2));
 
